4df1574b95153e6aa1f1f3922a5a313274a0bfbf
Juan Pablo Cuello <juan.cuello@globant.com>
2020-09-09 11:38:32 -0300

    cambiando el yarn lock para q no consuma más nexus :) version 2

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
-import RoomsContainer from "../components/RoomsContainer";
 import mediosDePago from "../images/img-tarjetas.png";
 
 const Contacto = () => {
